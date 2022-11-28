f84c12b39a7d00f68cb5318d1ddbf4b51d29949e
Fabergames1 <faber.games@hotmail.com>
2022-11-28 05:05:09 -0300

    first commit

---
+++ COBOL.cbl
@@ -0,0 +1,16 @@
+      ******************************************************************
+      * Author:FABRICIO PERRONE
+      * Date:14/11/2022
+      * Purpose:ESTUDO
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. COBOL.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "OLA EU SOU FABRICIO!"
+            STOP RUN.
+       END PROGRAM COBOL.
